daae5e78372e8b57d404b6547cbbe67c80c1fa85
pawel <paweljarosz002@gmail.com>
2025-11-29 19:55:58 +0100

    update

--- kernels/misc/subarray_sum.cu
+++ kernels/misc/subarray_sum.cu
@@ -1,16 +1,9 @@
 #include <cuda_runtime.h>
 #include <thrust/device_ptr.h>
 #include <thrust/reduce.h>
-#include <cub/cub.cuh>
 
 void solve(const int* input, int* output, int N, int S, int E) {
-
-    int* d_temp_storage = nullptr;
-    size_t temp_storage_bytes = 0;
-
-    // hacky way for now :P
-    cub::DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, input+S, output, E-S+1);
-    cudaMalloc(&d_temp_storage, temp_storage_bytes);
-
-    cub::DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, input+S, output, E-S+1);
+    thrust::device_ptr<const int> dev_ptr(input);
+    int sum = thrust::reduce(dev_ptr + S, dev_ptr + E + 1);
+    cudaMemcpy(output, &sum, sizeof(int), cudaMemcpyHostToDevice);
 }
